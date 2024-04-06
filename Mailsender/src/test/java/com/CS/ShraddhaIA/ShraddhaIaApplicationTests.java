edda23bee2a3f3a5e644d280342747b0ccad09ff
Axiom <63239974+Axiom0599@users.noreply.github.com>
2024-04-06 11:21:04 +0530

    Add files via upload

---
+++ Mailsender/src/test/java/com/CS/ShraddhaIA/ShraddhaIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CS.ShraddhaIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShraddhaIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
